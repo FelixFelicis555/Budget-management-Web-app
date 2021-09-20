47ffcf4658b3821f50f510da6dd89095e371934f
Pavanesh <78358679+FelixFelicis555@users.noreply.github.com>
2021-09-21 00:28:45 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path,include
+from app1 import views
+app_name='app1'
+urlpatterns = [
+    path('register',views.register,name='register'),
+    path('login',views.user_login,name='login'),
+    path('special',views.special,name='special'),
+    path('logout',views.user_logout,name='logout'),
+    path('search',views.user_search,name='search'),
+    path('transaction',views.make_transaction,name='transaction'),
+    path('profile',views.user_profile,name='profile'),
+    path('viewprofile',views.user_profile_view,name='viewprofile'),
+    path('notification',views.show_notification,name='notification'),
+    path('delete',views.delete_account,name='delete'),
+    path('admin',views.admin_notification,name='admin')
+]
